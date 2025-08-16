ab1d200ef90d102e90e9d4dcfeb4cc14a56a4363
Oswaldo Sierra <oswaldosierra2019@gmail.com>
2025-08-16 12:59:45 -0500

    Agrego ProyectBeer con archivos planos

---
+++ ProyectBeer/src/com/edu/uptc/handlingBeer/Interface/IActionsFile.java
@@ -0,0 +1,10 @@
+package com.edu.uptc.handlingBeer.Interface;
+
+import com.edu.uptc.handlingBeer.Enums.ETypeFile;
+
+public interface IActionsFile {
+	public void loadFile(ETypeFile eTypeFile);
+	public void dumpFile(ETypeFile eTypeFile);
+	
+
+}
---
+++ ProyectBeer/src/com/edu/uptc/handlingBeer/Persistence/FilePlain.java
@@ -0,0 +1,69 @@
+package com.edu.uptc.handlingBeer.Persistence;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import com.edu.uptc.handlingBeer.Constants.*;
+import com.edu.uptc.handlingBeer.config.*;
+
+public class FilePlain {
+	protected Config config;
+	
+	public FilePlain() {
+		config = Config.getInstance();
+	}
+	
+	private String readFile(String rutaNombre) {
+		StringBuilder contenido = new StringBuilder();
+        try {
+            FileReader fr = new FileReader(rutaNombre);
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                contenido.append(linea)
+                	.append(CommonConstants.BREAK_LINE);
+            }
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        return contenido.toString();
+	}
+	
+	
+	public void writeFile(String rutaNombre, String content) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaNombre))) {
+			writer.write(content);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+	}
+	
+	
+	
+	protected List<String> reader(String rutaNombre){
+		List<String> output = new ArrayList<>();
+		StringTokenizer tokens = new StringTokenizer(
+				this.readFile(rutaNombre), 
+				CommonConstants.BREAK_LINE);
+		while (tokens.hasMoreElements()) {
+			output.add(tokens.nextToken());	
+		}
+		return output;
+	}
+	
+	protected void writer(String rutaNombre, List<String> file){
+		StringBuilder strContent = new StringBuilder();
+		file.forEach(record -> strContent.append(record)
+				.append(CommonConstants.BREAK_LINE));
+		writeFile(rutaNombre, strContent.toString());
+	}
+	
+}
